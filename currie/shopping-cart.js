cde7318f65ea3a5f86908a2279aa2e1226a80276
curriedav <curriedav@gmail.com>
2013-10-22 17:29:57 -0700

    completes shopping-cart.js

--- currie/shopping-cart.js
+++ currie/shopping-cart.js
@@ -1,25 +1,50 @@
 var shoppingCart = {
-  add: function(itemObject) {
-  	this.item = itemObject;
-  },
-  // remove: function(itemObject, quantity) {
+	cart: [],
+	idCounter: 0,
+	
+	add: function(itemObject) {
+		itemObject.id = this.idCounter;
+		this.idCounter ++;
+		this.cart.push(itemObject);
+		return itemObject.id;
+  	},
+	remove: function(itemId, quantity) {
+		for (var i = shoppingCart.cart.length - 1; i >= 0; i--) {
+				if (shoppingCart.cart[i].id === itemId && shoppingCart.cart[i].count === quantity) {
+					shoppingCart.cart.splice(i, 1);
+				}
+				else if (shoppingCart.cart[i].id === itemId) {
+				shoppingCart.cart[i].count -= quantity;
+			}
 
-  // },
-  list: function() {
-  	if (this.length === 0) {
-  		console.log("Your shopping cart is empty.")
-  	} else {
-  		console.log("Here are the contents of your shopping cart:")
-  		
-  		for (var i = this.length - 1; i >= 0; i--) {
-  		console.log(this.count, "X", this.description)
+		};
+
+		// Check through all of the objects in the array 'cart' (forEach)
+		// then for any object that has an ID equal to 'itemId' remove the quantity from its count property
+		// if quantity removed equals or excceeds the count delete the object.
+
+  	},
+  	list: function() {
+	  	if (this.cart.length === 0) {
+	  		return "Your shopping cart is empty.";
+	  	} else {
+	  		console.log("Here are the contents of your shopping cart:");
+	  		
+	  		var listArray = [];
+	  		
+	  		for (var i = this.cart.length - 1; i >= 0; i--) {
+	  			listArray[i] = this.cart[i].count + " X " + this.cart[i].description;	
+	  		};
+	  	return listArray;
+	  	}	
+  	},    
+  	total: function() {
+  		var total = 0;
+  		for (var i = this.cart.length - 1; i >= 0; i--) {
+  			total += this.cart[i].price * this.cart[i].count;
   		};
-  	}	
-  },    
-  // total: function() {
-  	
-  // },
-
+  		return total.toFixed(2);
+  	},
 
 };
 
@@ -39,11 +64,12 @@ var item4 = shoppingCart.add({description: "Monsters Eat Whiny Children",
                               count: 1,
                               price: 11.07});
 
-//shoppingCart.remove(item3, 6);
-//shoppingCart.remove(item4, 1);
+shoppingCart.remove(item3, 6);
+shoppingCart.remove(item4, 1);
 
-console.log(shoppingCart.item);
+
 console.log(shoppingCart.list());
+
 
 /* Should print:
 [
@@ -53,6 +79,6 @@ console.log(shoppingCart.list());
 ]
 */
 
-// console.log(shoppingCart.total());
+console.log(shoppingCart.total());
 
 // Should print: 169.84
